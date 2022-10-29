556963a7b1a9429e87db0e479fda6ae25f380f25
itsuki-jp <itsuki19601@gmail.com>
2022-10-30 07:40:52 +0900

    hatena

--- puyopuyo/index.js
+++ puyopuyo/index.js
@@ -6,6 +6,10 @@ class Puyo {
         this.main = main;
         this.dropSpeed = dropSpeed;
     }
+    move(x, y) {
+        this.x += x;
+        this.y += y;
+    }
 }
 
 class Board {
@@ -39,6 +43,70 @@ class Board {
     }
     createPuyoPuyo() {
         const x = Math.floor(Math.random() * this.x);
+        const colour = ['red', 'blue', 'yellow', 'green'];
+        this.puyopuyo = [
+            new Puyo(x, 1, colour[Math.floor(Math.random() * colour.length)], true, 1),
+            new Puyo(x, 0, colour[Math.floor(Math.random() * colour.length)], false, 1)];
+    }
+
+    setPuyoPuyo() {
+        for (const puyo of this.puyopuyo) {
+            this.board[puyo.y][puyo.x] = puyo;
+        }
+    }
+    isMovable(dx, dy) {
+        let res = true;
+        for (const puyo of this.puyopuyo) {
+            const [x, y] = [puyo.x + dx, puyo.y + dy];
+            if (this.isOnBoard(x, y) && this.board[y][x] === this.empty) {
+                continue;
+            }
+            res = false;
+        }
+        return res;
+    }
+
+    movePuyo(dx, dy, drop = false) {
+        if (drop && !this.isMovable(dx, dy)) {
+            this.setPuyoPuyo();
+            this.createPuyoPuyo();
+            return;
+        }
+        for (const puyo of this.puyopuyo) {
+            puyo.x += dx;
+            puyo.y += dy;
+        }
+    }
+
+    drawPuyo() {
+        for (const puyo of this.puyopuyo) {
+            this.ctx.fillStyle = puyo.colour;
+            this.ctx.fillRect(puyo.x * this.canvasRatio, puyo.y * this.canvasRatio, this.canvasRatio, this.canvasRatio);
+        }
+    }
+    isOnBoard(x, y) {
+        return (0 <= x && x < this.x && 0 <= y && y < this.y);
+    }
+
+    searchDeletable(x, y, visited) {
+        const visitingArr = [[x, y]];
+        for (; visitingArr.length !== 0;) {
+            const [nx, ny] = visitingArr.pop();
+            if (this.isOnBoard(nx, ny)) {
+
+            }
+        }
+    }
+
+    isDeletableArr() {
+        const deleteChain = [];
+        const visited = [];
+        for (let i = 0; i < this.y; i++) { visited.push(Array(this.x).fill(0)) }
+        for (let i = 0; i < this.y; i++) {
+            for (let j = 0; j < this.x; j++) {
+
+            }
+        }
     }
 }
 
@@ -54,6 +122,32 @@ function init() {
     const board = new Board(x, y, canvasRatio, canvas.getContext('2d'));
     board.initBoard();
     board.drawBoard();
+    board.createPuyoPuyo();
+
+    document.onkeydown = (e) => {
+        const code = e.code;
+        let [dx, dy] = [undefined, undefined];
+        if (code === 'ArrowDown') {
+            [dx, dy] = [0, 1];
+        }
+        if (code === 'ArrowLeft') {
+            [dx, dy] = [-1, 0];
+        }
+        if (code === 'ArrowRight') {
+            [dx, dy] = [1, 0];
+        }
+        if (dx !== undefined && board.isMovable(dx, dy)) {
+            board.movePuyo(dx, dy);
+            board.drawBoard();
+            board.drawPuyo();
+        }
+    }
+
+    setInterval(() => {
+        board.movePuyo(0, 1, drop = true)
+        board.drawBoard();
+        board.drawPuyo();
+    }, 1000);
 
 }
 init();
